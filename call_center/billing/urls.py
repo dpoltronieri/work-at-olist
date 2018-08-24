cc7898abe049488fff1b19768e94adc5ed7ddae1
Daniel Poltronieri <danppoltronieri@gmail.com>
2018-08-24 15:31:24 -0300

    Renamed the path to a more pleasing one

--- call_center/billing/urls.py
+++ call_center/billing/urls.py
@@ -4,7 +4,7 @@ from billing import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('get_post_calls/', views.get_post_calls.as_view(), name='get_post_calls'),
+    path('calls/', views.get_post_calls.as_view(), name='get_post_calls'),
     #path('snippets/<int:pk>/', views.SnippetDetail.as_view())
 ]
 
