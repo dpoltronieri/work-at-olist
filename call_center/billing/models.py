ab2e12b386e3e4a1b424416f064aa5d9e5431ba1
Daniel Poltronieri <danppoltronieri@gmail.com>
2018-08-26 11:47:40 -0300

    Changing model made the tests fail as expected. To-do: Remove the deprecated id field from the tests.

--- call_center/billing/models.py
+++ call_center/billing/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 class Call(models.Model):
     # Explicit id
-    id = models.AutoField(primary_key=True)
+    #id = models.AutoField(primary_key=True, unique=True)
     # This default timezone seems like a good idea
     # IDEA: Put this in the documentation as a feature
     start = models.DateTimeField(default=timezone.now)
@@ -15,7 +15,7 @@ class Call(models.Model):
     #duration = models.DurationField(null=True)
     source = models.CharField(max_length=20)
     destination = models.CharField(max_length=20)
-    call_id = models.BigIntegerField(null=False, unique=True)
+    call_id = models.BigIntegerField(primary_key=True, unique=True)
 
     def __str__(self):
         return "id: {},call_id: {},start: {},end: {},source: {},destination: {}".format(
